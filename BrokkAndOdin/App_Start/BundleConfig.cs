65ed95874c07650ab3dc1914f5af3cb98d7a6ae1
aphramzo <im.t.wal@gmail.com>
2014-11-21 11:03:34 -0700

    Bootstrap sass source for variable usage. Housekeeping.

--- BrokkAndOdin/App_Start/BundleConfig.cs
+++ BrokkAndOdin/App_Start/BundleConfig.cs
@@ -33,7 +33,6 @@ namespace BrokkAndOdin
 					  "~/Scripts/home.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
 
 			bundles.Add(new StyleBundle("~/Content/gallery").Include(
