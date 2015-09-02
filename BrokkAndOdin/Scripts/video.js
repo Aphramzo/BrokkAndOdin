f0ac2b2d6bf5ce64138cf0e3961be797a89b74e5
im.t.wal <tyler.walborn@dts-global.com>
2015-09-02 13:40:23 -0600

    Shuffle and reverse controls on video page

--- BrokkAndOdin/Scripts/video.js
+++ BrokkAndOdin/Scripts/video.js
@@ -14,7 +14,10 @@
             video : $('video'),
             videoListContainer : document.getElementsByClassName("video-list")[0],
             linkArray : [],
-            currentVideoIndex : 0
+            currentVideoIndex: 0,
+            reverseButton: $('.btn-reverse'),
+            shuffleButton: $('.btn-shuffle'),
+            isReverse: false
         }
         this.fields.links = this.fields.videoListContainer.children;
         this.fields.numberOfVideos = this.fields.links.length;
@@ -34,6 +37,10 @@
         this.fields.video.on('mouseenter', this.onVideoMouseOver.bind(this));
 
         this.fields.video.on('mouseleave', this.onVideoMouseOut.bind(this));
+
+        this.fields.reverseButton.on('click', this.reversePlayOrder.bind(this));
+
+        this.fields.shuffleButton.on('click', this.shuffleVideos.bind(this));
 
         $('body').keydown(this.onDocumentKeyPress.bind(this));
 
@@ -63,7 +70,11 @@
         this.fields.video.removeAttr("controls");
     }
 
-    Video.prototype.playPreviousVideo = function() {
+    Video.prototype.playPreviousVideo = function (e, skipReverseCheck) {
+        if (this.fields.isReverse && !skipReverseCheck) {
+            this.playNextVideo(null, true);
+            return;
+        }
         this.unhighlightVideos();
         if (this.fields.currentVideoIndex == 0) {
             this.playVideo(this.fields.numberOfVideos - 1);
@@ -72,7 +83,11 @@
         }
     }
 
-    Video.prototype.playNextVideo = function () {
+    Video.prototype.playNextVideo = function (e, skipReverseCheck) {
+        if (this.fields.isReverse && !skipReverseCheck) {
+            this.playPreviousVideo(null, true);
+            return;
+        }
         this.unhighlightVideos();
         if ((this.fields.currentVideoIndex + 1) >= this.fields.numberOfVideos) {
             this.playVideo(0);
@@ -87,9 +102,64 @@
         }
     }
 
+    Video.prototype.reversePlayOrder = function() {
+        this.fields.isReverse = !this.fields.isReverse;
+        if (this.fields.isReverse) {
+            this.fields.reverseButton.addClass('btn-primary').removeClass('btn-default');
+        } else {
+            this.fields.reverseButton.addClass('btn-default').removeClass('btn-primary');
+        }
+    };
+
+    Video.prototype.shuffleVideos = function() {
+        this.shuffleLinks();
+        var links = this.fields.links;
+        $.each(links, function(i, link) {
+            var $link = $(link);
+            $link.insertAfter($link.siblings(':eq(' + i + ')'));
+            $link.data('index', i);
+        });
+        this.setupLinkList();
+        this.fields.videoListContainer.children[this.fields.currentVideoIndex].scrollIntoView();
+    }
+
+    Video.prototype.shuffleLinks = function () {
+        //TW: Copied shuffle algorithm from here: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        //get non dom dependant array
+        var array = $(this.fields.videoListContainer).children('a').map(function () {
+            return this;
+        });
+
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            //TW: TODO: I don't like having this here, but I cant think of another
+            //way to track the index of the currently playing video
+            if (randomIndex == this.fields.currentVideoIndex) {
+                this.fields.currentVideoIndex = currentIndex;
+            }
+            else if (currentIndex == this.fields.currentVideoIndex) {
+                this.fields.currentVideoIndex = randomIndex;
+            }
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        this.fields.links = array;
+    }
+
     Video.prototype.playVideo = function(index) {
         this.fields.videoListContainer.children[index].classList.add("currentvid");
-        if ($(this.fields.videoListContainer.children[index]).offset().top > 530) {
+        var offsetTop = $(this.fields.videoListContainer.children[index]).offset().top;
+        if (offsetTop > 530 || offsetTop < 0) {
             this.fields.videoListContainer.children[index].scrollIntoView();
         }
         
